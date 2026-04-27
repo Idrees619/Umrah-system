3dc9b2b32a4d92b6af545b29a00131c016c50711
Idrees619 <edris.abdulwaheed@gmail.com>
2026-04-27 15:22:23 +0300

    Delete backend web directory

--- backend web/db.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
-pool.connect((err,_,release) => { if(err) console.error('❌ DB Error:',err.message); else { console.log('✅ DB Connected'); release(); } });
-module.exports = pool;
